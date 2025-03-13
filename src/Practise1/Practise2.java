79e0420c4ec9f942f96804cb6b21d412a21c84a9
PawanBokolia <pawanbokolia1@gmail.com>
2025-03-13 16:08:17 +0530

    commit 13.3.25

--- src/Practise1/Practise2.java
+++ src/Practise1/Practise2.java
@@ -1,37 +1,40 @@
 package Practise1;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Practise2 {
 
 	public static void main(String []args) {
 		
-		int arr[]= {1,5,4,8,7,2,65,1,3,5};
+		int arr[]= {1,5,4,89,7,2,65,1,3,5,0};
 		int arr2[] = new int [arr.length];
-//		int j=0;
-//		Arrays.sort(arr);
-//		
-//		for(int i=0;i<arr.length;i++)
-//		{
-//			if(i==0 || arr[i] != arr[i-1])
-//			{
-//				arr2[j++]=arr[i];
-//			}
-//		}
-//		
-//		int arr3[]=Arrays.copyOf(arr2, j);
-//		System.out.println(Arrays.toString(arr3));
-//		
-//		
-//		int arr4[]=new int[arr3.length];
-//		for(int i=0;i<arr3.length;i++)
-//		{
-//			arr4[i] = arr3[arr3.length-1-i];
-//		}
-//		
-//		System.out.println(Arrays.toString(arr4));
-//		
-//		
+		int p = arr[0];
+		int j=0;
+
+		Arrays.sort(arr);
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			if(i==0 || arr[i] != arr[i-1])
+			{
+				arr2[j++]=arr[i];
+			}
+		}
+		
+		int arr3[]=Arrays.copyOf(arr2, j);
+		System.out.println(Arrays.toString(arr3));
+		
+		
+		int arr4[]=new int[arr3.length];
+		for(int i=0;i<arr3.length;i++)
+		{
+			arr4[i] = arr3[arr3.length-1-i];
+		}
+		
+		System.out.println(Arrays.toString(arr4));
+		
+		
 		HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
 		
 		for(int a:arr)
@@ -41,6 +44,27 @@ public class Practise2 {
 		
 		System.out.println( map.keySet());
 		System.out.println( map.values());
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			if(p<arr[i])
+			{
+				p=arr[i];
+			}
+		}
+		
+		System.out.println(p);
+	
+		for(int i=0;i<arr.length;i++)
+		{
+			if(p>arr[i])
+			{
+				p=arr[i];
+			}
+		}
+		
+		System.out.println(p);
+		
 		
 	}
 }

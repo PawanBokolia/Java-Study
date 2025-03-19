1cf2df39e959b1939026a990d937c007077d003f
PawanBokolia <pawanbokolia1@gmail.com>
2025-03-19 19:50:04 +0530

    19.3.25

--- src/Practise1/Newtest.java
+++ src/Practise1/Newtest.java
@@ -4,23 +4,22 @@ import java.util.Arrays;
 
 public class Newtest {
 
-	public static void main(String []args) {
-		
-		
-		int arr[] = {122,122,122,4,5,67,8,90,92};
-		int arr1[]= new int[arr.length];
-		int j=0;
-		Arrays.sort(arr);
-		
-		for(int i=0;i<arr.length;i++)
+	public static void main(String[] args) {
+
+		int arr[] = { 122, 122, 122, 4, 5, 67, 8, 90, 92, 1 };
+		int arr1[] = { 122, 122, 122, 7, 4, 5, 67, 8, 90, 92, 1, 8 };
+
+		for (int i = 0; i < arr.length; i++) 
 		{
-			if(i==0 || arr[i] !=arr[i-1])
+			for (int j = 0; j < arr1.length; j++) 
 			{
-				arr1[j++] = arr[i];
+				if (arr1[j] != arr[i]) 
+				{
+					arr1[i] = arr[i];
+				}
 			}
 		}
-		
-		int arr3[]= Arrays.copyOf(arr1, j);
-		System.out.println(Arrays.toString(arr3));
+
+		System.out.println(Arrays.toString(arr1));
 	}
 }

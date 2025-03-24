77ebfde2fc8e504a11058b4ac6ea7e6a9575bcf6
PawanBokolia <pawanbokolia1@gmail.com>
2025-03-24 18:44:35 +0530

    24.3.25

--- src/Practise1/AddingTwoArrays.java
+++ src/Practise1/AddingTwoArrays.java
@@ -1,6 +1,7 @@
 package Practise1;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class AddingTwoArrays {
 
@@ -9,17 +10,26 @@ public class AddingTwoArrays {
 		int arr[] = { 122, 122, 122, 4, 5, 67, 8, 90, 92, 1 };
 		int arr1[] = { 122, 122, 122, 7, 4, 5, 67, 8, 90, 92, 1, 8 };
 
-		for (int i = 0; i < arr.length; i++) 
+		ArrayList<Integer>list = new ArrayList<>();
+		
+		for(int a: arr)
 		{
-			for (int j = 0; j < arr1.length; j++) 
-			{
-				if (arr1[j] != arr[i]) 
-				{
-					arr1[i] = arr[i];
-				}
-			}
+			list.add(a);
 		}
 
-		System.out.println(Arrays.toString(arr1));
-	}
+		
+		
+		for(int a : arr1)
+		{
+			if(!list.contains(a))
+			{
+				list.add(a);
+			}
+		}
+		
+		Collections.sort(list);
+		System.out.println(list);
+		
+		
+		}
 }

e738a64e34b8cd9d05337799bfa4fcfae9a2d00f
PawanBokolia <pawanbokolia1@gmail.com>
2025-03-27 18:59:51 +0530

    27.3.25

--- src/practise/DuplicationFromArrayList.java
+++ src/practise/DuplicationFromArrayList.java
@@ -10,7 +10,7 @@ public class DuplicationFromArrayList {
 		
 	ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(1,2,1,6,7,8,99,9,9,9,5,6,24));	
 
-	HashMap<Integer,Integer> hashlist= new HashMap<Integer,Integer>();
+	HashMap<Integer,Integer> hashlist = new HashMap<Integer,Integer>();
 		
 	for(int i=0 ;i<list.size();i++)
 	{

1b09552756d18893c19bb745d6febceea36815f1
PawanBokolia <pawanbokolia1@gmail.com>
2025-04-11 13:35:35 +0530

    11.4.25

--- src/Practise1/Test123.java
+++ src/Practise1/Test123.java
@@ -1,52 +1,21 @@
 package Practise1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Test123 {
 
 	public static void main (String args[]) {
 		
-		int arr[]= {1,2,3,4,1,4,5,67,9};
-		int arr1[]=new int[arr.length];
-		int j=0;
-		Arrays.sort(arr);
+		int arr[]= {1,3,4,1,990,12,3,456,12,90};
 		
-		for(int i=0;i<arr.length;i++)
+		int lg=arr[0];
+		
+		for(int a : arr)
 		{
-			if(i==0 ||arr[i]!=arr[i-1])
+			if(lg < a )
 			{
-				arr1[j++]=arr[i];
+				lg=a;
 			}
 		}
-		
-		int arr2[]=Arrays.copyOf(arr1, j);
-		System.out.println(Arrays.toString(arr2));
-	
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		ArrayList<Integer> list2 = new ArrayList<>();
-	
-		for(int a :arr)
-		{
-			if(!list.contains(a))
-			{
-				list.add(a);
-			}
-		}
-		
-		System.out.println(list);
-
-		for(int i=0;i<list.size();i++)
-		{
-			list2.add(list.get(list.size()-1-i));
-		}
-		
-		
-		
-		
-		System.out.println(list2);
-		
+		 System.out.println(lg);
 		
 	}
 }

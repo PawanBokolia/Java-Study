dc98a5bc17d1cc9e18145dfa450612c0e253e987
PawanBokolia <pawanbokolia1@gmail.com>
2025-08-11 14:39:44 +0530

    office 11.8.25

--- src/study2/Practise1.java
+++ src/study2/Practise1.java
@@ -2,13 +2,17 @@ package study2;
 
 public class Practise1 {
 
-	public static void main(String  []args) {
+	int a,b,c;
+	
+	int test(int a )
+	{
+		return a = a+190;
+	}
 	
 	
-		
-		
-	
-	
-	
+	public static void main(String []args)
+	{
+		Practise1 p = new Practise1();
+		System.out.println(p.test(1));	
 	}
 }
